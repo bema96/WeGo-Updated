b83b1cf32dae9b5175bb10a3a4e18518de66de15
Miau <165878715+bema96@users.noreply.github.com>
2025-09-23 10:48:11 +0200

    slides v2: return string[]

--- server/src/routes/slideRoutes.ts
+++ server/src/routes/slideRoutes.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 import { getRecords } from '../controllers/slideController';
 
 const routes = Router();
+console.log("slides v2: returning string[]");
+
 routes.get('/', getRecords);
 
 export const slideRoutes = routes;
